1bd2a3c9801b06b59ab39fb9362d64edf04829a0
Rafael J. Rodriguez <rafase282@gmail.com>
2015-07-21 09:43:29 -0400

    Completed another problem

--- practice.js
+++ practice.js
@@ -196,11 +196,12 @@ var user = {
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
   //Code Here
-
+user.name = 'Tyler S. McGinnis';
+user.email =  'tyler.mcginnis@devmounta.in';
 //Now call the sayName method that's on the user object which will alert the users email
 
   //Code Here
-
+user.sayName();
 
 
 
